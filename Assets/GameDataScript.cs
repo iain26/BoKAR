3f85bbc0c5b9db69667b4e5436e92d3b4cfb9de0
iain26 <icotte200@caledonian.ac.uk>
2018-02-22 03:27:08 +0000

    Game Template

---
+++ Assets/GameDataScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameDataScript : MonoBehaviour {
+
+    public Scene MapScene;
+
+	// Use this for initialization
+	void Start ()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        GameObject[] GDarray = GameObject.FindGameObjectsWithTag("GameData");
+
+        if (GDarray.Length > 1)
+        {
+            Destroy(gameObject);
+        }
+	}
+
+    public void LoadMap()
+    {
+        //Application.LoadLevel("Map");
+        SceneManager.LoadScene(1);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Mgr.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mgr : MonoBehaviour {
+    
+    Ray ray;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.touchCount > 0)
+        {
+            ray = Camera.main.ScreenPointToRay(Input.touches[0].position);
+            RaycastHit hit = new RaycastHit();
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.tag == "ObjectOnMap")
+                {
+                    Application.LoadLevel(hit.collider.name + "Scene");
+                }
+            }
+        }
+    }
+}
